63a7cc759f691674a5b0c7b2db555029f1449171
Dylan Bettendroffer <dylan.bettendroffer@gmail.com>
2024-07-08 15:48:31 +0200

    Initial home-manager config

--- flake.nix
+++ flake.nix
@@ -4,19 +4,30 @@
   inputs = {
 
     nixpkgs.url = "nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
   };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs = { self, nixpkgs, home-manager , ... }:
     let
+      system = "x86_64-linux";
       lib = nixpkgs.lib;
+      pkgs = nixpkgs.legacyPackages.${system};
     in
     {
       nixosConfigurations = {
         nixos = lib.nixosSystem {
-          system = "x86_64-linux";
+	  inherit system;
           modules = [ ./configuration.nix ];
         };
+      };
+
+      homeConfigurations = {
+        dymdym = home-manager.lib.homeManagerConfiguration {
+	  inherit pkgs;
+          modules = [ ./home.nix ];
+        };       
       };
     };
 }
