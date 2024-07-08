7f6901544a17535b808b2f4fcfbd9eca0d373c95
Dylan Bettendroffer <dylan.bettendroffer@gmail.com>
2024-07-08 17:17:57 +0200

    started Hyprland config

--- home.nix
+++ home.nix
@@ -15,9 +15,9 @@
   # release notes.
   home.stateVersion = "24.05"; # Please read the comment before changing.
 
-#  modules = [ 
-#    ./hyprland.nix 
-#  ];
+  imports = [ 
+    ./hyprland.nix 
+  ];
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
@@ -36,20 +36,17 @@
     # '')
   ];
 
+  programs = {
+    fish.enable = true;
+    starship.enable = true;
+  };
+
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
 
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-    
+    ".config/hypr/hyprland.conf".source = ./hyprland.conf;
+   
   };
 
   # Home Manager can also manage your environment variables through
@@ -69,12 +66,7 @@
   #  /etc/profiles/per-user/dymdym/etc/profile.d/hm-session-vars.sh
   #
   home.sessionVariables = {
-    # EDITOR = "emacs";
-  };
-
-  programs = {
-    fish.enable = true;
-    starship.enable = true;
+     EDITOR = "nvim";
   };
 
   # Let Home Manager install and manage itself.
