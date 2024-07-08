fa828e2616d56a81ecb979359c32075bc88892a6
Dylan Bettendroffer <dylan.bettendroffer@gmail.com>
2024-07-08 14:25:13 +0200

    Initial flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Dymdym's dotfiles";
+
+  inputs = {
+
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      lib = nixpkgs.lib;
+    in
+    {
+      nixosConfigurations = {
+        nixos = lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [ ./configuration.nix ];
+        };
+      };
+    };
+}
