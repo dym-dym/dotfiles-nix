273a6fd7dac577b293df36b1780d91b4f87a61dc
Dylan Bettendroffer <dylan.bettendroffer@gmail.com>
2024-07-09 14:46:23 +0200

    fixed typo

--- user/wm/hypr/hyprland.nix
+++ user/wm/hypr/hyprland.nix
@@ -151,10 +151,11 @@
           )
           10)
       );
+    bindm = [
+      "SUPER, mouse:272, movewindow"
+      "SUPER, mouse:273, resizewindow"
+    ];
   };
 
-  bindm = [
-    "SUPER, mouse:272, movewindow"
-    "SUPER, mouse:273, resizewindow"
-  ];
+
 }
