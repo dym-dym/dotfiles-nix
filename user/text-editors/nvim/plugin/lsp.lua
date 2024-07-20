cf7a40c7b50e5db5f20dd496a24c85b8a6356191
Dylan Bettendroffer <dylan.bettendroffer@gmail.com>
2024-07-20 23:55:39 +0200

    Rust related fixups

--- user/text-editors/nvim/plugin/lsp.lua
+++ user/text-editors/nvim/plugin/lsp.lua
@@ -32,3 +32,8 @@ require('lspconfig').nil_ls.setup {
     on_attach = on_attach,
     capabilities = capabilities,
 }
+
+require('lspconfig').rust_analyzer.setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+}
