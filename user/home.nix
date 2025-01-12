409bb89705cd30fb32a7a6e3db7aba2411fb041f
Dylan Bettendroffer <dylan.bettendroffer@gmail.com>
2025-01-12 01:19:48 +0100

    Kanagawa base16 theme

--- user/home.nix
+++ user/home.nix
@@ -1,7 +1,8 @@
 { config, lib, pkgs, inputs, ... }:
 let
   system = "x86_64-linux";
-  theme = "tokyo-night-dark";
+  # theme = "tokyo-night-dark";
+  theme = "kanagawa";
   polarity = "dark";
   wallpaper = "forrest.png";
 
