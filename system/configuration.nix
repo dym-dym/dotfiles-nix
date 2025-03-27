a8a259994e2af5615724046acf2bab9f9ff9c394
Dylan Bettendroffer <dylan.bettendroffer@gmail.com>
2025-03-27 17:31:02 +0000

    March 27th update

--- system/configuration.nix
+++ system/configuration.nix
@@ -119,9 +119,9 @@
 
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-      version = "570.124.04";
+      version = "570.133.07";
 
-      sha256_64bit = "sha256-G3hqS3Ei18QhbFiuQAdoik93jBlsFI2RkWOBXuENU8Q=";
+      sha256_64bit = "sha256-LUPmTFgb5e9VTemIixqpADfvbUX1QoTT2dztwI3E3CY=";
       sha256_aarch64 = lib.fakeSha256;
       openSha256 = lib.fakeSha256;
       settingsSha256 = "sha256-vWnrXlBCb3K5uVkDFmJDVq51wrCoqgPF03lSjZOuU8M=";
