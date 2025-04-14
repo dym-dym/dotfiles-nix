943c91b64311782d35ecab30c3dcec6df9873ec7
Dylan Bettendroffer <dylan.bettendroffer@gmail.com>
2025-04-14 14:11:06 +0100

    Removing neodev

--- modules/programs/neovim/nvim/plugin/lsp.lua
+++ modules/programs/neovim/nvim/plugin/lsp.lua
@@ -26,7 +26,7 @@ end
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
-require('neodev').setup()
+-- require('neodev').setup()
 
 lspconfig = require('lspconfig')
 
