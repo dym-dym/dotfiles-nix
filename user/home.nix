2df9a5304a44c796be05e32f735eea07852d0279
Dylan Bettendroffer <dylan.bettendroffer@gmail.com>
2025-04-30 01:22:22 +0100

    Mimeapps list update

--- user/home.nix
+++ user/home.nix
@@ -130,6 +130,54 @@ in
     };
   };
 
+  xdg = {
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "inode/directory" = ["pcmanfm.desktop"]; # Directories
+        "text/plain" = ["neovim.desktop"]; # Plain text
+        # "application/vnd.openxmlformats-officedocument.wordprocessingml.document" = ["onlyoffice-desktopeditors.desktop"]; # .docx
+        # "application/vnd.openxmlformats-officedocument.presentationml.presentation" = ["onlyoffice-desktopeditors.desktop"]; # .pptx
+        "application/pdf" = ["zathura.desktop"]; # .pdf
+        "application/zip" = ["xarchiver.desktop"];
+        "text/*" = ["neovim.desktop"]; # Any text files
+        "video/*" = ["mpv.desktop"]; # Any video files
+        # "x-scheme-handler/https" = ["librewolf.desktop"]; # Links
+        # "x-scheme-handler/http" = ["librewolf.desktop"]; # Links
+        # "x-scheme-handler/mailto" = ["librewolf.desktop"]; # Links
+        # "x-scheme-handler/https" = ["zen-beta.desktop"]; # Links
+        # "x-scheme-handler/http" = ["zen-beta.desktop"]; # Links
+        # "x-scheme-handler/mailto" = ["zen-beta.desktop"]; # Links
+        "image/*" = ["feh.desktop"]; # Images
+        "image/png" = ["feh.desktop"];
+        "image/jpeg" = ["feh.desktop"];
+        "x-scheme-handler/http" = ["userapp-Zen6RAP32.desktop" "zen-beta.desktop"];
+        "x-scheme-handler/https" = ["userapp-Zen6RAP32.desktop" "zen-beta.desktop"];
+        "x-scheme-handler/mailto" = ["zen-beta.desktop"];
+        "x-scheme-handler/chrome" = ["userapp-Zen6RAP32.desktop" "zen-beta.desktop"];
+        "text/html" = ["userapp-Zen6RAP32.desktop" "zen-beta.desktop"];
+        "application/x-extension-htm" = ["userapp-Zen6RAP32.desktop" "zen-beta.desktop"];
+        "application/x-extension-html" = ["userapp-Zen6RAP32.desktop" "zen-beta.desktop"];
+        "application/x-extension-shtml" = ["userapp-Zen6RAP32.desktop" "zen-beta.desktop"];
+        "application/xhtml+xml" = ["userapp-Zen6RAP32.desktop" "zen-beta.desktop"];
+        "application/x-extension-xhtml" = ["userapp-Zen6RAP32.desktop" "zen-beta.desktop"];
+        "application/x-extension-xht" = ["userapp-Zen6RAP32.desktop" "zen-beta.desktop"];
+      };
+      associations.added = {
+        "x-scheme-handler/http" = ["userapp-Zen6RAP32.desktop" "zen-beta.desktop"];
+        "x-scheme-handler/https" = ["userapp-Zen6RAP32.desktop" "zen-beta.desktop"];
+        "x-scheme-handler/chrome" = ["userapp-Zen6RAP32.desktop" "zen-beta.desktop"];
+        "text/html" = ["userapp-Zen6RAP32.desktop" "zen-beta.desktop"];
+        "application/x-extension-htm" = ["userapp-Zen6RAP32.desktop" "zen-beta.desktop"];
+        "application/x-extension-html" = ["userapp-Zen6RAP32.desktop" "zen-beta.desktop"];
+        "application/x-extension-shtml" = ["userapp-Zen6RAP32.desktop" "zen-beta.desktop"];
+        "application/xhtml+xml" = ["userapp-Zen6RAP32.desktop" "zen-beta.desktop"];
+        "application/x-extension-xhtml" = ["userapp-Zen6RAP32.desktop" "zen-beta.desktop"];
+        "application/x-extension-xht" = ["userapp-Zen6RAP32.desktop" "zen-beta.desktop"];
+      };
+    };
+  };
+
   texlive.enable = true;
 
   services.udiskie.enable = true;
