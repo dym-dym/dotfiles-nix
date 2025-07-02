a1e7baae30a7b725b8f1f48fc4c33e52ee5dc657
Dylan Bettendroffer <dylan.bettendroffer@gmail.com>
2025-07-02 20:06:11 +0100

    July 2nd Update

--- system/configuration.nix
+++ system/configuration.nix
@@ -17,8 +17,8 @@
     ];
 
   # Bootloader.
-  boot.kernelPackages = pkgs.linuxPackages_6_14;
-  # boot.kernelPackages = pkgs.linuxPackages_latest;
+  # boot.kernelPackages = pkgs.linuxPackages_6_14;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.loader = {
     systemd-boot.enable = true;
     systemd-boot.configurationLimit = 5;
@@ -121,9 +121,9 @@
 
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-      version = "575.57.08";
+      version = "575.64";
 
-      sha256_64bit = "sha256-KqcB2sGAp7IKbleMzNkB3tjUTlfWBYDwj50o3R//xvI=";
+      sha256_64bit = "sha256-6wG8/nOwbH0ktgg8J+ZBT2l5VC8G5lYBQhtkzMCtaLE=";
       sha256_aarch64 = lib.fakeSha256;
       openSha256 = lib.fakeSha256;
       settingsSha256 = "sha256-vWnrXlBCb3K5uVkDFmJDVq51wrCoqgPF03lSjZOuU8M=";
