593befaeb307a3173d9b645cb091e0b21c7cd95e
Dylan Bettendroffer <dylan.bettendroffer@gmail.com>
2026-02-10 12:54:50 +0000

    Nushell aliases fix

--- modules/programs/nushell/default.nix
+++ modules/programs/nushell/default.nix
@@ -41,12 +41,48 @@
          prepend /home/myuser/.apps |
          append /usr/bin/env
          )
+
+# alias the built-in ls command to ls-builtins
+        alias ls-builtin = ls
+
+# List the filenames, sizes, and modification times of items in a directory.
+        def ls [
+            --all (-a),         # Show hidden files
+            --long (-l),        # Get all available columns for each entry (slower; columns are platform-dependent)
+            --short-names (-s), # Only print the file names, and not the path
+            --full-paths (-f),  # display paths as absolute paths
+            --du (-d),          # Display the apparent directory size ("disk usage") in place of the directory metadata size
+            --directory (-D),   # List the specified directory itself instead of its contents
+            --mime-type (-m),   # Show mime-type in type column instead of 'file' (based on filenames only; files' contents are not examined)
+            --threads (-t),     # Use multiple threads to list contents. Output will be non-deterministic.
+            ...pattern: glob,   # The glob pattern to use.
+        ]: [ nothing -> table ] {
+            let pattern = if ($pattern | is-empty) { [ '.' ] } else { $pattern }
+            (ls-builtin
+                --all=$all
+                --long=$long
+                --short-names=$short_names
+                --full-paths=$full_paths
+                --du=$du
+                --directory=$directory
+                --mime-type=$mime_type
+                --threads=$threads
+                ...$pattern
+            ) | sort-by type name -i
+        }
+
+        def v [
+          ...pattern: glob
+        ] : [ nothing -> nothing ] {
+          let patter = if ($pattern | is-empty) { [ '.' ] } else { $pattern }
+          (nvim ...$pattern)
+        }
          '';
 
         shellAliases = {
-          vi = "nvim";
-          vim = "nvim";
-          v = "nvim";
+          # vi = "nvim";
+          # vim = "nvim";
+          # v = "nvim";
         };
       };
 
