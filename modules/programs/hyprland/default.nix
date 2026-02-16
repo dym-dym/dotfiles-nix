fba4391d878d1cfc771dd0fe29969bec2582933b
Dylan Bettendroffer <dylan.bettendroffer@gmail.com>
2026-02-16 15:51:53 +0000

    screen stuff

--- modules/programs/hyprland/default.nix
+++ modules/programs/hyprland/default.nix
@@ -22,7 +22,7 @@ in
 
 	  wayland.windowManager.hyprland.settings = {
 
-	    monitor = ["eDP-1,1920x1200@60,0x0,1" "HDMI-A-2,800x600@60,1920x0,0.5"];
+	    monitor = ["eDP-1,1920x1200@60,0x0,1" "HDMI-A-1,1920x1080@144,0x-1200,1"];
 
 	    source = "~/.config/hypr/mocha.conf";
 
