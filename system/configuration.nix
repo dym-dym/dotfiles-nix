3ab80350280c8cc8ee1c821a5a2dd7f83a51c426
Dylan Bettendroffer <dylan.bettendroffer@gmail.com>
2026-05-30 19:22:04 +0100

    Cachy LTS entry

--- system/configuration.nix
+++ system/configuration.nix
@@ -34,11 +34,17 @@
       };
 
       extraEntries = ''
-/CachyOS
+/+CachyOS
+//Cachy OS
   protocol: linux
   path: uuid(13f1af99-7bf1-4b82-a97d-a9e780265198):/vmlinuz-linux-cachyos
   cmdline: rootflags=subvol=/@ root=UUID=71f0dfa0-c386-44f8-93df-b3ad9ae5affb quiet udev.log_level=3 systemd.show_status=auto nvidia-drm.modeset=1 nvidia-drm.fbdev=1 splash loglevel=3 lsm=landlock,yama,bpf rw
   module_path: uuid(13f1af99-7bf1-4b82-a97d-a9e780265198):/initramfs-linux-cachyos.img
+//Cachy OS LTS
+  protocol: linux
+  path: uuid(13f1af99-7bf1-4b82-a97d-a9e780265198):/vmlinuz-linux-cachyos-lts
+  cmdline: rootflags=subvol=/@ root=UUID=71f0dfa0-c386-44f8-93df-b3ad9ae5affb quiet udev.log_level=3 systemd.show_status=auto nvidia-drm.modeset=1 nvidia-drm.fbdev=1 splash loglevel=3 lsm=landlock,yama,bpf rw
+  module_path: uuid(13f1af99-7bf1-4b82-a97d-a9e780265198):/initramfs-linux-cachyos-lts.img
 '';
 
     };
