6fc0e74f7dfeb0511f533da44acda55442c68327
Dylan Bettendroffer <dylan.bettendroffer@gmail.com>
2026-07-07 01:12:42 +0100

    Fixed Mimeapps for pdfs

--- home/xdg.nix
+++ home/xdg.nix
@@ -5,18 +5,10 @@
       defaultApplications = lib.mkForce {
         "inode/directory" = ["pcmanfm.desktop"]; # Directories
         "text/plain" = ["neovim.desktop"]; # Plain text
-        # "application/vnd.openxmlformats-officedocument.wordprocessingml.document" = ["onlyoffice-desktopeditors.desktop"]; # .docx
-        # "application/vnd.openxmlformats-officedocument.presentationml.presentation" = ["onlyoffice-desktopeditors.desktop"]; # .pptx
-        "application/pdf" = lib.mkForce ["zathura.desktop"]; # .pdf
+        "application/pdf" = lib.mkForce ["org.pwmt.zathura-pdf-mupdf.desktop"]; # .pdf
         "application/zip" = ["xarchiver.desktop"];
         "text/*" = ["neovim.desktop"]; # Any text files
         "video/*" = ["mpv.desktop"]; # Any video files
-        # "x-scheme-handler/https" = ["librewolf.desktop"]; # Links
-        # "x-scheme-handler/http" = ["librewolf.desktop"]; # Links
-        # "x-scheme-handler/mailto" = ["librewolf.desktop"]; # Links
-        # "x-scheme-handler/https" = ["zen-beta.desktop"]; # Links
-        # "x-scheme-handler/http" = ["zen-beta.desktop"]; # Links
-        # "x-scheme-handler/mailto" = ["zen-beta.desktop"]; # Links
         "image/*" = ["feh.desktop"]; # Images
         "image/png" = ["feh.desktop"];
         "image/jpeg" = ["feh.desktop"];
@@ -43,7 +35,10 @@
         "application/xhtml+xml" = ["userapp-Zen6RAP32.desktop" "zen-beta.desktop"];
         "application/x-extension-xhtml" = ["userapp-Zen6RAP32.desktop" "zen-beta.desktop"];
         "application/x-extension-xht" = ["userapp-Zen6RAP32.desktop" "zen-beta.desktop"];
-        "application/pdf" = ["zathura.desktop"]; # .pdf
+        "application/pdf" = ["org.pwmt.zathura-pdf-mupdf.desktop"]; # .pdf
+      };
+      associations.removed = lib.mkForce {
+        "application/pdf" = ["gimp.desktop"]; # .pdf
       };
     };
   };
